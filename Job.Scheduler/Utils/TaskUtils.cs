a1923fdfc3cb601cd223facab476b17dd0c10018
Antoine Aflalo <git@aaflalo.me>
2020-12-23 17:14:45 -0500

    Fix task leakage

--- Job.Scheduler/Utils/TaskUtils.cs
+++ Job.Scheduler/Utils/TaskUtils.cs
@@ -10,22 +10,16 @@ namespace Job.Scheduler.Utils
         /// Wait for the given delay or a cancellation token to expire.
         ///
         /// Doesn't trigger an exception
-        /// <see href="https://stackoverflow.com/questions/30739788/cancel-task-delay-without-exception-or-use-exception-to-control-flow"/>
         /// </summary>
         public static async Task WaitForDelayOrCancellation(TimeSpan delay, CancellationToken token)
         {
-            var tcs = new TaskCompletionSource<object>(TaskCreationOptions.RunContinuationsAsynchronously);
-
-            var cts = new CancellationTokenSource();
-            await using var _ = token
-                .Register(o =>
-                    {
-                        tcs.SetResult(null);
-                        cts.Cancel();
-                    },
-                    null);
-
-            await Task.WhenAny(tcs.Task, Task.Delay(delay, cts.Token));
+            try
+            {
+                await Task.Delay(delay, token);
+            }
+            catch (OperationCanceledException)
+            {
+            }
         }
     }
 }
