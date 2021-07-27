0484803cf58a83cf046c5c8f135f56ea5785ec0b
Antoine Aflalo <Belphemur@users.noreply.github.com>
2021-07-26 21:31:59 -0400

    feat(BackoffRetry): Add 2 different backoff retry using the new delay between retries

---
+++ Job.Scheduler/Job/Action/BackoffRetry.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Job.Scheduler.Job.Action
+{
+    public class BackoffRetry : IRetryAction
+    {
+        private readonly int? _maxRetries;
+        private readonly Func<int, TimeSpan> _retryStrategy;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="retryStrategy">Implement your own strategy for delay between retries based on the current retry</param>
+        /// <param name="maxRetries">null to always retry</param>
+        public BackoffRetry(Func<int, TimeSpan> retryStrategy, int? maxRetries)
+        {
+            _maxRetries = maxRetries;
+            _retryStrategy = retryStrategy;
+        }
+
+        public bool ShouldRetry(int currentRetry)
+        {
+            if (!_maxRetries.HasValue)
+            {
+                return true;
+            }
+
+            return currentRetry < _maxRetries;
+        }
+
+        public TimeSpan? GetDelayBetweenRetries(int currentRetry)
+        {
+            return _retryStrategy(currentRetry);
+        }
+    }
+}
---
+++ Job.Scheduler/Job/Action/ExponentialBackoffRetry.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Job.Scheduler.Job.Action
+{
+    public class ExponentialBackoffRetry : BackoffRetry
+    {
+        public ExponentialBackoffRetry(TimeSpan baseDelay, int? maxRetries) : base(currentRetry => TimeSpan.FromMilliseconds(baseDelay.TotalMilliseconds * Math.Pow(2, currentRetry)), maxRetries)
+        {
+        }
+    }
+}
